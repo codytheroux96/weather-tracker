fd5905353aab5aa75aa5144cc1f024dda93e65c1
cdt96 <cody.theroux3@gmail.com>
2022-07-10 11:40:17 -0400

    layed out functions I'll need in js and set some global variables

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+let apiURL = "";
+let searchHistory = [];
+let previousSearch = "";
+
+let getWeather = () => {
+
+};
+
+let submitHandler = () => {
+
+};
+
+let displayWeather = () => {
+
+};
+
+let saveSearchHistory = () => {
+
+};
+
+let renderSearchHistory = () => {
+
+};
+
