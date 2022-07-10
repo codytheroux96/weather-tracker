aacf21ac402470aa544583185def970b48f20378
cdt96 <cody.theroux3@gmail.com>
2022-07-10 12:12:37 -0400

    started my getWeather function

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,22 @@ let searchHistory = [];
 let previousSearch = "";
 
 let getWeather = () => {
-
+  let apiUrl = "";
+  fetch(apiUrl)
+      .then(function(response) {
+          if (response.ok) {
+              response.json().then(function(data) {
+                  displayWeather(data);
+              });
+          } else {
+              alert("Error!");
+          }
+      })  
+      .catch(function(error) {
+          alert("Cannot connect to OpenWeather");
+      })
 };
+
 
 let submitHandler = () => {
 
